4d29274485abcbe6b6faf5bbc9426a891dff8231
AndreLuisPLuz <andreluispluz@gmail.com>
2024-09-03 16:47:33 -0300

    feat: added PageContent component.

---
+++ src/components/PageContent/index.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+import { Page } from "./style";
+
+const PageContent = (): ReactNode => {
+    return (
+        <Page/>
+    );
+};
+
+export default PageContent;
---
+++ src/components/PageContent/style.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Page = styled.main`
+    top: 50px;
+
+    min-height: calc(100vh - 50px);
+    width: 100vw;
+
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+`;
+
+export { Page };
