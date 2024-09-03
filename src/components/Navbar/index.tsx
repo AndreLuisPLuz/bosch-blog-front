da2a0bb1f3c4a179439b0a57d2b113735d89f68e
AndreLuisPLuz <andreluispluz@gmail.com>
2024-09-03 13:57:34 -0300

    feat: created basic component structure.

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+import { Nav } from "./style";
+
+const Navbar = (): ReactNode => {
+    return (
+        <Nav>
+            Teste
+            Teste
+            Teste
+        </Nav>
+    );
+};
+
+export default Navbar;
---
+++ src/components/Navbar/style.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Nav = styled.nav`
+    position: fixed;
+    top: 0;
+    left: 0;
+
+    height: 40px;
+    width: 100vw;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+`;
+
+export { Nav };
