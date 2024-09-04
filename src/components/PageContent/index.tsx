663ee028353e9aadffa447545743c34c83aca4f9
AndreLuisPLuz <andreluispluz@gmail.com>
2024-09-04 13:50:18 -0300

    build: added usehooks as a dependency.

--- src/components/PageContent/index.tsx
+++ src/components/PageContent/index.tsx
@@ -1,9 +1,15 @@
 import { ReactNode } from "react";
 import { Page } from "./style";
 
-const PageContent = (): ReactNode => {
+type PageContentProps = {
+    children?: ReactNode | ReactNode[]
+};
+
+const PageContent = (props: PageContentProps): ReactNode => {
     return (
-        <Page/>
+        <Page>
+            { props.children }
+        </Page>
     );
 };
 
