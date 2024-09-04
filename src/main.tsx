8d4363c1e8d4e3722a12e5559f484ae447505681
AndreLuisPLuz <andreluispluz@gmail.com>
2024-09-04 14:10:04 -0300

    feat: created a simple login page, doing tests on modal.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import HomePage from './pages/Home'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import LoginTest from './pages/LoginTest'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage/>,
+  },
+  {
+    path: "/auth/login",
+    element: <LoginTest/>,
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HomePage/>
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
---
+++ src/pages/LoginTest/index.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+import LoginModal from "../../modules/authentication/pages/LoginModal";
+
+const LoginTest = (): ReactNode => {
+    return (
+        <LoginModal>
+
+        </LoginModal>
+    );
+};
+
+export default LoginTest;
